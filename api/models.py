c811814d3324f48f8e546e9843b6f404994aaa7f
toran billups <toranb@gmail.com>
2015-07-29 21:22:22 -0500

    added models/initial migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    description = models.CharField(max_length=100)
+
+class Session(models.Model):
+    name = models.CharField(max_length=100)
+    tags = models.ManyToManyField(Tag)
+
+class Speaker(models.Model):
+    name = models.CharField(max_length=100)
+    session = models.ForeignKey(Session, blank=True, null=True, related_name='speakers')
